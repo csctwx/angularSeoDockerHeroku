d6b057fd613d04963a7a83b2b6e59ce4539df344
wenxiong.tu <wenxiong.tu@aircanada.ca>
2020-10-10 19:38:47 -0400

    init angular app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'firstComponent', pathMatch: 'full'},
+      {path: 'firstComponent', component: FirstComponent},
+      {path: 'secondComponent', component: SecondComponent}
+    ])
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { MenuComponent } from './menu/menu.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
